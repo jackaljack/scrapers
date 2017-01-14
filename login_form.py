3ecbe63e6292a8b46b24480125e3508a10af8d06
jackaljack <jackdebidda@gmail.com>
2017-01-14 18:41:24 +0100

    Add request to login form

---
+++ login_form.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from urllib.parse import urlencode
+import requests
+from http import cookiejar
+import lxml.html
+
+
+LOGIN_EMAIL = 'example@webscraping.com'
+LOGIN_PASSWORD = 'example'
+LOGIN_URL = 'http://example.webscraping.com/user/login'
+COUNTRY_URL = 'http://example.webscraping.com/edit/United-Kingdom-239'
+
+
+def login_basic():
+    print('login_basic')
+    data = {'email': LOGIN_EMAIL, 'password': LOGIN_PASSWORD}
+    encoded_data = urlencode(data)
+    req = requests.get(LOGIN_URL, data=encoded_data)
+    print(req.url)
+    print('Status code: {}\n'.format(req.status_code))
+
+
+def login_formkey():
+    print('login_formkey')
+    req = requests.get(LOGIN_URL)
+    html = req.text
+    data = parse_form(html)
+    data['email'] = LOGIN_EMAIL
+    data['password'] = LOGIN_PASSWORD
+    encoded_data = urlencode(data)
+    req = requests.get(LOGIN_URL, data=encoded_data)
+    print(req.url)
+    print('Status code: {}\n'.format(req.status_code))
+
+
+def login_cookies():
+    print('login_cookies')
+    cj = cookiejar.CookieJar()
+    req = requests.get(LOGIN_URL, cookies=cj)
+    html = req.text
+    data = parse_form(html)
+    data['email'] = LOGIN_EMAIL
+    data['password'] = LOGIN_PASSWORD
+    encoded_data = urlencode(data)
+    req = requests.get(LOGIN_URL, data=encoded_data)
+    print(req.url)
+    print('Status code: {}\n'.format(req.status_code))
+
+
+def parse_form(html):
+    """extract all input properties from the form"""
+    tree = lxml.html.fromstring(html)
+    data = {}
+    for e in tree.cssselect('form input'):
+        if e.get('name'):
+            data[e.get('name')] = e.get('value')
+    return data
+
+
+def main():
+    login_basic()
+    login_formkey()
+    login_cookies()
+
+
+if __name__ == '__main__':
+    main()
